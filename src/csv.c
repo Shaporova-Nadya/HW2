620c651c22b6840e044239ed9688086e9a89dfc5
Nadya Shaporova <saporovanada@gmail.com>
2026-03-15 22:59:51 +0300

    Added csv.c

---
+++ src/csv.c
@@ -0,0 +1,38 @@
+#include "csv.h"
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE 8192
+
+int isNumeric(const char* s)
+{
+    if(!s || !*s){
+        return 0;
+    }
+    char* endptr;
+   
+    strtod(s, &endptr);
+
+    if (s == endptr) return 0;
+
+    while (isspace((unsigned char)*endptr)) {
+        endptr++;
+    }
+
+    return *endptr == '\0';
+}
+
+void printLine(FILE* out, int* widths, int cols, char fill)
+{
+    fprintf(out, "+");
+    for(int i = 0; i < cols; i++){
+        for(int j = 0; j < widths[i] + 2; j++){
+            fputc(fill, out);
+        }
+        fprintf(out, "+");
+    }
+    fprintf(out, "\n");
+}
