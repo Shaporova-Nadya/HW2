21d2562a9942e56599d35986a849e7006b35c0f6
Nadya Shaporova <saporovanada@gmail.com>
2026-03-15 23:00:20 +0300

    Added csv.h

---
+++ src/csv.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+
+#define MAX_LINE 8192
+
+// isNumeric checks if a string is a number
+int isNumeric(const char* s);
+
+// printLine draws pseudographics
+void printLine(FILE* out, int* widths, int cols, char fill);
