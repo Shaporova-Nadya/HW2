c5eb5fac7937fadf832219604a52b11299e4601e
Nadya Shaporova <saporovanada@gmail.com>
2026-03-15 23:05:31 +0300

    Fixed style

--- src/csv.c
+++ src/csv.c
@@ -2,21 +2,22 @@
 
 #include <ctype.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_LINE 8192
 
 int isNumeric(const char* s)
 {
-    if(!s || !*s){
+    if (!s || !*s) {
         return 0;
     }
     char* endptr;
-   
+
     strtod(s, &endptr);
 
-    if (s == endptr) return 0;
+    if (s == endptr)
+        return 0;
 
     while (isspace((unsigned char)*endptr)) {
         endptr++;
@@ -28,8 +29,8 @@ int isNumeric(const char* s)
 void printLine(FILE* out, int* widths, int cols, char fill)
 {
     fprintf(out, "+");
-    for(int i = 0; i < cols; i++){
-        for(int j = 0; j < widths[i] + 2; j++){
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < widths[i] + 2; j++) {
             fputc(fill, out);
         }
         fprintf(out, "+");
