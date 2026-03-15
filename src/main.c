c421c4d5fb6b582d6e0be6d35c93e5de053b5954
Nadya Shaporova <saporovanada@gmail.com>
2026-03-16 02:02:50 +0300

    fixed

--- src/main.c
+++ src/main.c
@@ -30,7 +30,7 @@ int main(void)
                 if (temp == NULL) {
                     free(widths);
                     fclose(in);
-                    return;
+                    return 1;
                 }
                 widths = (int*)temp;
                 widths[currentCol] = 0;
@@ -70,7 +70,7 @@ int main(void)
                 cell[comma - ptr] = '\0';
                 ptr = comma + 1;
             } else {
-                int len = strlen(ptr);
+                int len = (int)strlen(ptr);
                 strncpy(cell, ptr, len);
                 cell[len] = '\0';
                 ptr += len;
