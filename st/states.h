c383ff6084febc3279da2173b72ec096daf2e76a
Nadya Shaporova <saporovanada@gmail.com>
2026-04-01 22:24:41 +0300

    fixed style

--- st/states.h
+++ st/states.h
@@ -1,37 +1,37 @@
 #pragma once
 
 typedef struct Edge {
-    int num;           //Номер города, куда ведет дорога
-    int len;           //длина пути
-    struct Edge* next; //указатель на следующее ребро этого города
+    int num;
+    int len;
+    struct Edge* next;
 } Edge;
 
 typedef struct City {
-    struct Edge* head; //Указатель на первое ребро в списке смежности
+    struct Edge* head;
 } City;
 
 typedef struct HeapNode {
-    int dist;    //Расстояние
-    int cityNum; //Номер города
+    int dist;
+    int cityNum;
 } HeapNode;
 
 typedef struct MinHeap {
-    HeapNode* data; //Массив, где лежат элементы кучи
-    int size;       //Текущее количество элементов
-    int capacity;   //Максимальный размер
+    HeapNode* data;
+    int size;
+    int capacity;
 } MinHeap;
 
-//Добавляет новое ребро в список смежности
+/* Добавляет новое ребро в список смежности */
 void addEdge(City* graph, int u, int v, int len);
 
-//Освобождает память, выделенную под граф
+/* Освобождает память, выделенную под граф */
 void freeGraph(City* graph, int n);
 
-//Инициализация бинарной кучи
+/* Инициализация бинарной кучи */
 MinHeap* initHeap(int maxSize);
 
-//Добавляет новый элемент в кучу
+/* Добавляет новый элемент в кучу */
 void push(MinHeap* h, int dist, int cityNum);
 
-//Извлекает элемент с минимальным расстоянием из кучи
+/* Извлекает элемент с минимальным расстоянием из кучи */
 HeapNode popMin(MinHeap* h);
