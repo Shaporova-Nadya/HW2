bcd4ebb1cf8a626d76bf7740d65a1b8c77efed2e
Nadya Shaporova <saporovanada@gmail.com>
2026-04-16 03:35:57 +0300

    Corrected style

--- src/main.c
+++ src/main.c
@@ -5,17 +5,17 @@
 int main(void)
 {
     Table* table = createTable();
-    if (!table){
+    if (!table) {
         return -1;
     }
 
-    if (readCSV(table, "input.csv") != 0){
+    if (readCSV(table, "input.csv") != 0) {
         deleteTable(table);
         return -1;
     }
 
     FILE* out = fopen("output.txt", "w");
-    if (!out){
+    if (!out) {
         deleteTable(table);
         return -1;
     }
