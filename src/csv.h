1a783448cb658ecd8916ba4b99d3dbc9cab1ff73
Nadya Shaporova <saporovanada@gmail.com>
2026-04-16 21:01:23 +0300

    Deleted static functions from .h

--- src/csv.h
+++ src/csv.h
@@ -3,12 +3,7 @@
 
 #define MAX_LINE 8192
 
-typedef struct Table {
-    char*** data;
-    int rows;
-    int cols;
-    int* widths;
-} Table;
+typedef struct Table Table;
 
 // isNumeric checks if a string is a number
 int isNumeric(const char* s);
@@ -21,15 +16,6 @@ Table* createTable(void);
 
 // deletes table
 void deleteTable(Table* t);
-
-// reads all lines from file
-static char** readLines(const char* filename, int* outRowCount);
-
-// determines the max number of cols
-static int getMaxCols(char** lines, int rowCount);
-
-// fills the table from an array of rows
-static void fillTable(Table* t, char** lines, int rowCount, int maxCols);
 
 // calculates widths
 void computeWidths(Table* t);
