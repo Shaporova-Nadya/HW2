e7d88ff2c26ed9a64ab95c351881718014c531f0
Nadya Shaporova <saporovanada@gmail.com>
2026-04-18 03:13:26 +0300

    Changed style

--- src/csv.c
+++ src/csv.c
@@ -118,9 +118,7 @@ static char** readLines(const char* filename, int* outRowCount)
 
         if (strlen(buf) == MAX_LINE - 1 && buf[MAX_LINE - 2] != '\n') {
             int ch;
-            while ((ch = fgetc(in)) != '\n' && ch != EOF) {
-                
-            }
+            while ((ch = fgetc(in)) != '\n' && ch != EOF) {}
         }
     }
     fclose(in);
